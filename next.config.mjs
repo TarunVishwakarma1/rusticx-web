4b0ef179abc6da4d117667690f4a98082242054e
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-04-26 02:26:32 +0530

    Fix next.js config for Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output:"export",
+  output: "export",
+  basePath: process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : "",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
